c191135a9b9ff1cfe7ff614d67ff59bca7d4add9
Patryk Waśko <patryk.wasko@gmail.com>
2019-03-03 22:47:05 +0100

    T-2 Correct description method

--- spec/requests/api/v1/projects_spec.rb
+++ spec/requests/api/v1/projects_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe API::V1::Users, type: :request do
+RSpec.describe API::V1::Projects, type: :request do
   let!(:user) { FactoryBot.create :user }
 
   before do
